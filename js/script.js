4e7f4c9334b057923a8c5451f87769f52b54e0cf
andreagorno <andreagorno98@gmail.com>
2021-05-20 14:11:02 +0200

    scheletro pagina

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Milestone 1:
+// Replica della grafica (immagine in allegato) con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse;
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto, ricavandoli dall'array contacts qui allegato
+
+var app = new Vue({
+    el: "#root",
+
+    data: {
+
+    },
+
+    methods: {
+
+    },
+});
